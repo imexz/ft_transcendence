24c21728eb384a87ef38801c87f706c85539bd3c
tobbel42 <tobiasdegrossmann@gmx.de>
2022-09-02 18:46:30 +0200

    added the example Vue.js App + Docker file to support it

---
+++ src/frontend/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld
+      msg="Welcome to Your Vue.js + TypeScript App"
+      insult="Ihr Pimmel 🖤🖤🖤"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class HomeView extends Vue {}
+</script>
