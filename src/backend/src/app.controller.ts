337526efd5a01a9217936e1f41a82a4cf305063a
Samuel Hackbeil <shackbei@1-E-5.42heilbronn.de>
2022-09-06 09:52:17 +0200

    add data base

--- src/backend/src/app.controller.ts
+++ src/backend/src/app.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard'
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('test')
+  login(@Request() req): string {
+    return "test"; //retunr JWT
   }
 
 //   @UseGuards(LocalAuthGuard)
