9339795cd1870138199441afe1d0a3daee929634
tobbel42 <tobiasdegrossmann@gmx.de>
2022-09-07 17:17:07 +0200

    login

--- src/frontend/src/views/LoginView.vue
+++ src/frontend/src/views/LoginView.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
-    <a href="http://localhost:3000/auth/login">Login</a>
+    <a href='http://localhost:3000/auth/login'>login</a>
+    <!-- <button @click=authenticateFT>Auth</button> -->
+    <br/>
   </div>
 </template>
+
+<script lang ="ts">
+  import { Vue } from 'vue-class-component';
+  import VueAxios from 'axios';
+
+
+
+  export default class LoginView extends Vue {
+    jwt: any = '404'
+    authenticateFT(): void {
+      VueAxios
+        .get('http://localhost:3000/auth/login')
+        .then(response => (this.jwt = response))
+      console.log(this.jwt)
+    }
+  }
+</script>
