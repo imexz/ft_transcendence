770c996e64e3df1f4dbe95e08d3c44f6f0ecc909
tobbel42 <tobiasdegrossmann@gmx.de>
2022-09-07 18:39:54 +0200

    frontend now uses volumes

--- src/frontend/src/views/LoginView.vue
+++ src/frontend/src/views/LoginView.vue
@@ -1,24 +1,13 @@
 <template>
   <div>
-    <a href='http://localhost:3000/auth/login'>login</a>
-    <!-- <button @click=authenticateFT>Auth</button> -->
-    <br/>
+    <a href='http://localhost:3000/auth/login'>42 AUTH</a>
   </div>
 </template>
 
 <script lang ="ts">
   import { Vue } from 'vue-class-component';
-  import VueAxios from 'axios';
-
-
 
   export default class LoginView extends Vue {
-    jwt: any = '404'
-    authenticateFT(): void {
-      VueAxios
-        .get('http://localhost:3000/auth/login')
-        .then(response => (this.jwt = response))
-      console.log(this.jwt)
-    }
+
   }
 </script>
