4f33dcd1235814e376d13b177a8d9ffcc0c1813a
Samuel Hackbeil <shackbei@1-E-5.42heilbronn.de>
2022-09-08 12:02:27 +0200

    add credentials

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -4,7 +4,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: "http://localhost:8080"});
+  app.enableCors({origin: "http://localhost:8080", credentials: true});
   app.use(cookieParser());
   await app.listen(3000);
 }
