3eac4ef4062c569aa7c7615a9dfbfe3889990e9d
tobbel42 <tobiasdegrossmann@gmx.de>
2022-09-12 18:04:45 +0200

    works

--- src/frontend/src/views/ProfileView.vue
+++ src/frontend/src/views/ProfileView.vue
@@ -1,22 +1,17 @@
 <template>
-  <img :src=ProfilePicUrl alt='Profile Pic'>
-  <MatchHistory></MatchHistory>
+  <img :src='ProfilePicUrl' alt='Profile Pic'>
 </template>
 
 <script lang="ts">
-
   import { Options, Vue } from 'vue-class-component';
-
-
 
   export default class ProileView extends Vue {
     ProfilePicUrl: string = ""
     mounted() {
+      console.log(this.$store.getters.getUser)
       this.ProfilePicUrl = this.$store.getters.getUser.avatar_url;
     }
   }
-
-
 </script>
 
 <style>
