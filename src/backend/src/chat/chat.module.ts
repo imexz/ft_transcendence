60744e5192d941f7cd93dd78a687e91e91a81251
Samuel Hackbeil <shackbei@1-E-5.42heilbronn.de>
2022-09-13 16:05:00 +0200

    merge

--- src/backend/src/chat/chat.module.ts
+++ src/backend/src/chat/chat.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { chatroom } from '../chatroom/chatroom.entity';
 import { message } from '../message/message.entity';
 import { User } from '../users/entitys/user.entity';
+import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 
 @Module({
 	imports: [TypeOrmModule.forFeature([message, User, chatroom])],
-  providers: [ChatService],
+  providers: [ChatService, ChatGateway],
   exports: [TypeOrmModule, ChatService],
 })
 export class ChatModule {}
