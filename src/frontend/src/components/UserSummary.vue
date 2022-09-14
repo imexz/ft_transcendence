89ba960e33aa74e1db51011d76827933f901d509
tobbel42 <tobiasdegrossmann@gmx.de>
2022-09-14 16:49:48 +0200

    add Friend

--- src/frontend/src/components/UserSummary.vue
+++ src/frontend/src/components/UserSummary.vue
@@ -2,20 +2,32 @@
   <div>
     <img :src="user.avatar_url" alt="Avatar">
     <span>{{ user.unique_name }}</span>
+    <button @click="addFriend" >AddFriend</button>
   </div>
 </template>
 
 <script lang="ts">
   import { Options, Vue } from 'vue-class-component';
-  import User from '../models/user';
+  import VueAxios from 'axios';
 
   @Options ({
     props : {
-      user: User
+      user: Object
     }
   })
 
   export default class UserSummary extends Vue {
+    addFriend(): void {
+      VueAxios({
+        url: '/users/addFriend',
+        baseURL: 'http://localhost:3000',
+        method: 'POST',
+        withCredentials: true,
+        data: {"id" : this.user.id},
+      })
+        .then()
+        .catch()
+    }
   }
 </script>
 
@@ -26,15 +38,23 @@
     border-radius: 50%;
     object-fit: cover;
     vertical-align: middle;
+    padding: 10px;
+    display: inline-block;
   }
   span {
     vertical-align: middle;
-    padding: 50px;
+    /* padding: 50px; */
+    display: inline-block;
     font-size: 30px;
+    width: 300px;
   }
   div {
+    text-align: left;
     border: 5px solid;
     border-image-slice: 1;
     border-image-source: linear-gradient(var(--ft_pink), var(--ft_blue));
+  }
+  button {
+    vertical-align: middle;
   }
 </style>
