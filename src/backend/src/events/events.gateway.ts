6f0b8ac45acb54a976c3400baceeef590fc272a8
Samuel Hackbeil <shackbei@1-E-5.42heilbronn.de>
2022-09-15 14:42:52 +0200

    remove prints

--- src/backend/src/events/events.gateway.ts
+++ src/backend/src/events/events.gateway.ts
@@ -16,26 +16,21 @@ export class EventsGateway {
 
   @SubscribeMessage('moveLeftUp')
   handleMoveLeftUp(): void {
-    console.log("moveLeftUp");
-    
 	this.gameService.movePaddleUp(true);
   }
   @SubscribeMessage('moveRightUp')
   handleMoveRightUp(): void {
-    console.log("moveRightUp");
 	this.gameService.movePaddleUp(false);
   }
   
 
   @SubscribeMessage('moveLeftDown')
   handleMoveLeftDown(): void {
-    console.log("moveLeftDown");
 	this.gameService.movePaddleDown(true);
   }
 
   @SubscribeMessage('moveRightDown')
   handleMoveRightDown(): void {
-    console.log("moveLeftDown");
 	this.gameService.movePaddleDown(false);
   }
 }
