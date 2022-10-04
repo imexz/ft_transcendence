be41cbc10c633a058f02b2865c05a739fa97554f
tobbel42 <tobiasdegrossmann@gmx.de>
2022-10-04 20:53:55 +0200

    Friendlist now dynamic

--- src/frontend/src/store/index.ts
+++ src/frontend/src/store/index.ts
@@ -3,22 +3,25 @@ import { Vue } from 'vue-class-component'
 import { createStore, storeKey } from 'vuex'
 import User from '@/models/user';
 import router from '@/router';
+import VueAxios from 'axios';
+import { API_URL } from '@/defines';
 
 
-export interface validated {
-  validated : boolean
-}
-export interface user {
-  user : User
+
+export interface State {
+  validated: boolean
+  user: User
+  socket: null
+  friendsList: User[] | null
 }
 
 const storage = localStorage.getItem('user')
 const user = storage?JSON.parse(storage):null;
 const initialState = user?
-  {validated: true, user: user, socket: null}:
-  {validated: false, user: null,  socket: null}
+  {validated: true, user: user, socket: null, friendsList: null}:
+  {validated: false, user: null,  socket: null, friendsList: null};
 
-export default createStore({
+export default createStore<State>({
 
   state: initialState,
   getters: {
@@ -30,8 +33,10 @@ export default createStore({
     },
     getSocket(state) {
       return state.socket
-    }
-
+    },
+    getFriends(state) {
+      return state.friendsList
+    } 
   },
   mutations: {
     logOut(state) {
@@ -46,6 +51,17 @@ export default createStore({
     },
     resetAvatar(state) {
       state.user.avatar_url = state.user.avatar_url_42intra;
+    },
+    setFriendsList(state, friendsList) {
+      state.friendsList = friendsList;
+    },
+    addFriend(state, user) {
+      state.friendsList?.push(user)
+    },
+    removeFriend(state, id) { //todo make better
+      const index = state.friendsList?.findIndex(element => element._id == id)
+      if (index != -1)
+        state.friendsList?.splice(state.friendsList?.findIndex(element => element._id == id) , 1)
     }
   },
   actions: {
@@ -58,6 +74,16 @@ export default createStore({
     logIn({ commit }, user) {
       commit('logIn', user);
       localStorage.setItem('user', JSON.stringify(user));
+    },
+    getFriendsList({ commit }) {
+      VueAxios({
+        url: '/users/friends',
+        baseURL: API_URL,
+        method: 'GET',
+        withCredentials: true,
+      })
+      .then(response => { commit('setFriendsList', response.data)})
+      .catch()
     }
   },
   modules: {
