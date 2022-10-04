b70f2e372ef0b86261e8c2d931e64d09556cc3b3
Maximilian Strantz <max.str@web.de>
2022-10-04 18:58:26 +0200

    202210041855 PongGame.vue change in export default

--- src/backend/src/game/game.controller.ts
+++ src/backend/src/game/game.controller.ts
@@ -2,17 +2,22 @@ import { Controller, Sse, Param } from '@nestjs/common';
 import { GameService } from './game.service';
 import { map, interval, Observable } from 'rxjs';
 import { Game } from './game.entities/game.entity';
+import { Ball } from './game.entities/ball.entity';
+import { Paddle } from './game.entities/paddle.entity';
+import { Score } from './game.entities/score.entity';
+import { GameSetup } from './game.entities/setup.entity';
 
-interface MessageEvent {
-	data: Game;
-}
+// interface MessageEvent {
+// 	data: Game | undefined;
+// }
 
 @Controller('game')
 export class GameController {
 	constructor(private readonly gameService: GameService) {}
 
-	@Sse('sse/:gameid')
-	sse(@Param('gameid') gameid: number): Observable<MessageEvent> {
-		return interval(100).pipe(map((_) => ({data: this.gameService.getData(gameid)})));
-	}
+	// change to websockets: each game gets its own room, sockets of players join this room. send data to backend with set interval
+	// @Sse('sse/:gameid')
+	// sse(@Param('gameid') gameid: number): Observable<MessageEvent> {
+	// 	return interval(100).pipe(map((_) => ({data: this.gameService.getData(gameid)})));
+	// }
 }
--- src/frontend/src/components/Game/PongGame.vue
+++ src/frontend/src/components/Game/PongGame.vue
@@ -22,118 +22,122 @@
   import ScoreCounter from '@/components/Game/ScoreCounter.vue'
   import io from "socket.io-client";
   import { API_URL } from '@/defines';
-
-  @Options({
-    components: {
-      ScoreCounter
-    }
-  })
-
-export default class PongGame extends Vue {
-	gameId: string = ""
-	socket: any
-	context: any = {}
-	eventSource: any = {}
-	position: any = {
-		x: 0,
-		y: 0
-	}
-	side: string = ""
-
-	created() { // always called when Component is initialized (e.g. on refresh)
-		console.log("in created");
-		this.socket = io(API_URL, {
-			auth: (cb) => {
-				cb ({id: this.$store.getters.getUser.id })
-			}
-		});
-		this.socket.on('gameInfo', (data) => {
-			console.log("event gameInfo received");
-			console.log(data.gameId);
-			console.log(data.side);
-			this.gameId = data.gameId;
-			this.side = data.side;
-			console.log("received GameId: %s, side: %s", this.gameId, this.side);
-			this.eventSource = new EventSource(API_URL + "/game/sse/" + this.gameId);
-			document.addEventListener('keydown', (event) => {
-				if (this.side === "left") {
-					if (event.key == 'w') {
-						console.log(event.key);
-						this.paddleLeftUp();
-					}
-					else if (event.key == 's') {
-						console.log(event.key);
-						this.paddleLeftDown();
-					}
-				} else if (this.side === "right") {
-					if (event.key == 'ArrowUp') {
-						console.log(event.key);
-						this.paddleRightUp();
-					}
-					else if (event.key == 'ArrowDown') {
-						console.log(event.key);
-						this.paddleRightDown();
-					}
-				}
-			}, false);
-		});
-		this.socket.emit('checkGame', (cb: boolean) => {
-			if (!cb) {
-				console.log("calling checkQueue");
-				this.socket.emit('checkQueue');
-			}
-		});
-	}
-
-	mounted() {
-		console.log("in mounted");	
-		this.eventSource.onmessage = (raw_data:  any) => {
-			console.log("event received");
-
-			// console.log(raw_data);
-			let data = JSON.parse(raw_data.data);
-			console.log(data);
-			this.context = (this.$refs.game as any).getContext("2d");
-			this.context.fillStyle = "#FFFFFF";
-			this.position.x = data.ball.position.x;
-			this.position.y = data.ball.position.y;
-			this.context.clearRect(0, 0, (this.$refs.game as any).width, (this.$refs.game as any).height);
-			this.context.beginPath();
-			this.context.arc(this.position.x, this.position.y, data.ball.radius, 0, 2 * Math.PI);
-			this.context.fill();
-			this.drawPaddles(data);
-		};
-	}
-
-	beforeDestory() {
-		console.log("in beforeDestroy");
-		this.eventSource.close();
-		delete this.eventSource;
-		this.socket.close();
-		delete this.socket;
-		delete this.position;
-		delete this.context;
-		// delete this.gameId;
-	}
-
-	drawPaddles(data: any) {
-		this.context.fillStyle = "#FFFFFF";
-		this.context.fillRect(data.paddleLeft.position.x, data.paddleLeft.position.y, data.paddleLeft.width, data.paddleLeft.height);
-		this.context.fillRect(data.paddleRight.position.x, data.paddleRight.position.y, data.paddleRight.width, data.paddleRight.height);
-	}
-	paddleLeftUp() {
-		this.socket.emit('moveLeftUp');
-	}
-	paddleLeftDown() {
-		this.socket.emit('moveLeftDown');
-	}
-	paddleRightUp() {
-		this.socket.emit('moveRightUp');
-	}
-	paddleRightDown() {
-		this.socket.emit('moveRightDown');
-	}
-}
+  import { defineComponent } from 'vue';
+		
+  export default defineComponent({
+  	data () {
+  		return {
+  			gameId: "" as string,
+  			socket: null as any,
+  			context: null as any,
+  			eventSource: null as any,
+  			position: {
+  				x: 0 as number,
+  				y: 0 as number,
+  			},
+  			side: "" as string,
+  		}
+  	},
+  	components: {
+  		ScoreCounter
+  	},
+  	created() { // always called when Component is initialized (e.g. on refresh)
+  		console.log("in created");
+  		this.socket = io(API_URL, {
+  			auth: (cb: any) => {
+  				cb ({id: this.$store.getters.getUser.id })
+  			}
+  		});
+  		this.socket.on('gameInfo', (data: any) => {
+  			console.log("event gameInfo received");
+  			this.gameId = data.gameId;
+  			this.side = data.side;
+  			console.log("received GameId: %s, side: %s", this.gameId, this.side);
+  			this.eventSource = new EventSource(API_URL + "/game/sse/" + this.gameId);
+  			document.addEventListener('keydown', (event) => {
+  				if (this.side === "left") {
+  					if (event.key == 'w') {
+  						console.log(event.key);
+  						this.paddleLeftUp();
+  					}
+  					else if (event.key == 's') {
+  						console.log(event.key);
+  						this.paddleLeftDown();
+  					}
+  				} else if (this.side === "right") {
+  					if (event.key == 'ArrowUp') {
+  						console.log(event.key);
+  						this.paddleRightUp();
+  					}
+  					else if (event.key == 'ArrowDown') {
+  						console.log(event.key);
+  						this.paddleRightDown();
+  					}
+  				}
+  			}, false);
+  		});
+  		this.socket.emit('checkGame', (cb: boolean) => {
+  		if (!cb) {
+  			console.log("calling checkQueue");
+  			this.socket.emit('checkQueue');
+  		}
+  		});
+  		console.log("leaving created");
+  	},
+  	mounted() {
+  		if (this.eventSource === null) return;
+  		console.log("in mounted");
+  		this.eventSource.onmessage = (raw_data: any) => {
+  			console.log("event received");
+  			if (raw_data.data === undefined) {
+  				console.log("data undefined");
+  				return;
+  			}
+  			// console.log(raw_data);
+  			let data = JSON.parse(raw_data.data);
+  			console.log(data);
+  			this.context = (this.$refs.game as any).getContext("2d");
+  			this.context.fillStyle = "#FFFFFF";
+  			this.position.x = data.ball.position.x;
+  			this.position.y = data.ball.position.y;
+  			this.context.clearRect(0, 0, (this.$refs.game as any).width, (this.$refs.game as any).height);
+  			this.context.beginPath();
+  			this.context.arc(this.position.x, this.position.y, data.ball.radius, 0, 2 * Math.PI);
+  			this.context.fill();
+  			this.drawPaddles(data);
+  		};
+  		console.log("leaving mounted");
+  	},
+  	beforeDestory() {
+  		console.log("in beforeDestroy");
+  		this.eventSource.close();
+  		delete this.eventSource;
+  		this.socket.close();
+  		delete this.socket;
+  		// delete this.position;
+  		delete this.context;
+  		// delete this.gameId;
+  	},
+  	methods: {
+  		drawPaddles(data: any) {
+  			this.context.fillStyle = "#FFFFFF";
+  			this.context.fillRect(data.paddleLeft.position.x, data.paddleLeft.position.y, data.paddleLeft.width, data.paddleLeft.height);
+  			this.context.fillRect(data.paddleRight.position.x, data.paddleRight.position.y, data.paddleRight.width, data.paddleRight.height);
+  		},
+  		paddleLeftUp() {
+  			this.socket.emit('moveLeftUp');
+  		},
+  		paddleLeftDown() {
+  			this.socket.emit('moveLeftDown');
+  		},
+  		paddleRightUp() {
+  			this.socket.emit('moveRightUp');
+  		},
+  		paddleRightDown() {
+  			this.socket.emit('moveRightDown');
+  		},
+  	}
+  })	
 </script>
 
 <style scoped>
