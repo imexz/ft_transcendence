0f0106a91ad69c6e16d8fdd01c0cce0b56b9623d
tobbel42 <tobiasdegrossmann@gmx.de>
2022-10-13 15:58:54 +0200

    Polished the searchButton

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -15,6 +15,7 @@ import {
   faTableTennisPaddleBall,
   faBan,
   faXmark,
+  faChevronUp,
   } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import io from 'socket.io-client'
@@ -32,6 +33,7 @@ library.add(
   faBan,
   faTableTennisPaddleBall,
   faXmark,
+  faChevronUp
 )
 
 
