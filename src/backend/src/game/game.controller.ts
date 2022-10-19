32b3345513b5b9fb009a6ddcc537c5da4e37ba66
Maximilian Strantz <max.str@web.de>
2022-10-19 23:02:03 +0200

    202210192302

--- src/backend/src/game/game.controller.ts
+++ src/backend/src/game/game.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Sse, Param } from '@nestjs/common';
+import { Controller, Get, UseGuards, Param } from '@nestjs/common';
 import { GameService } from './game.service';
 import { map, interval, Observable } from 'rxjs';
 import { Game } from './game.entities/game.entity';
 import { Paddle } from './game.entities/paddle.entity';
 import { Score } from './game.entities/score.entity';
 import { GameSetup } from './game.entities/setup.entity';
+import { JwtAuthGuard } from 'src/auth/jwt-two/jwt-auth.guard';
 
 // interface MessageEvent {
 // 	data: Game | undefined;
@@ -14,9 +15,14 @@ import { GameSetup } from './game.entities/setup.entity';
 export class GameController {
 	constructor(private readonly gameService: GameService) {}
 
-	// change to websockets: each game gets its own room, sockets of players join this room. send data to backend with set interval
-	// @Sse('sse/:gameid')
-	// sse(@Param('gameid') gameid: number): Observable<MessageEvent> {
-	// 	return interval(100).pipe(map((_) => ({data: this.gameService.getData(gameid)})));
-	// }
+	@Get('spectate/:id')
+	@UseGuards(JwtAuthGuard)
+	spectate(@Param('id') playerId: number) {
+		const gameId = this.gameService.users.get(playerId.toString());
+		// TODO: get userId of spectator
+		// map spectator to gameId in gameService.users map
+		// redirect to PongGame.vue (?)
+		return gameId;
+	}
+
 }
