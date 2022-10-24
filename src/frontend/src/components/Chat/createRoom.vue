304a3112dafe9ea9e9b0c8d68ca9c05724118979
tobbel42 <tobiasdegrossmann@gmx.de>
2022-10-24 18:31:45 +0200

    style the chat and overhaul CreateRoomPopUp

--- src/frontend/src/components/Chat/createRoom.vue
+++ src/frontend/src/components/Chat/createRoom.vue
@@ -8,7 +8,7 @@
           <option>protected</option> 
         </select>
         <input v-if="access === 'protected'"  v-model="password" placeholder="Enter your password">
-        <button type="submit">Create Room </button>
+        <button type="submit">Create Room</button>
       </form>
     </div>    
 </template>
@@ -16,38 +16,69 @@
 <script lang="ts">
 import VueAxios from 'axios';
 import { API_URL } from '@/defines';
-import { ref } from 'vue'
+import { ref, defineComponent } from 'vue'
 
 
 
-export default {
-    props: ['TogglePopup'],
-    data() {
-        return {
-            name: '',
-            access: 'public',
-            password: ''
-        }
+export default defineComponent({
+  data() {
+      return {
+          name: '',
+          access: 'public',
+          password: ''
+      }
+  },
+  methods: {
+    createRoom(): void{
+      console.log("creatRoom");            
+      VueAxios({
+        url: '/chatroom/creat',
+        baseURL: API_URL,
+        method: 'POST',
+        withCredentials: true,
+        data: { room_name: this.name, access: this.access, password: this.password}
+      })
+        .then(response => {
+        console.log(response);
+        if(response != null)
+            console.log("success");
+            this.$emit('actions', 'success');
+        })
+        .catch(error => { this.$emit('actions', 'error') })
+      }
     },
-    methods: {
-        createRoom(): void{
-            console.log("creatRoom");            
-            VueAxios({
-                url: '/chatroom/creat',
-                baseURL: API_URL,
-                method: 'POST',
-                withCredentials: true,
-                data: { room_name: this.name, access: this.access, password: this.password}
-            })
-                .then(response => {
-                console.log(response);
-                if(response != null)
-                    console.log("succes");
-                    this.TogglePopup()
-                })
-                .catch(error => { this.$emit('error') })
-            }
-        },
-
-}
+})
 </script>
+
+<style scoped>
+  button {
+    color: var(--ft_cyan);
+    border: 1px solid var(--ft_cyan);
+    border-radius: 5px;
+    background-color: var(--ft_dark);
+    padding: 5px 8px;
+  }
+  button:active {
+    transform: translateY(1px);
+  }
+  button:hover {
+    color: var(--ft_dark);
+    background-color: var(--ft_cyan);
+  }
+
+  select {
+    color: var(--ft_cyan);
+    background-color: var(--ft_dark);
+    padding: 5px 8px;
+    border-color: var(--ft_cyan);
+    border-radius: 5px;
+  }
+
+  input{
+    color: var(--ft_cyan);
+    background-color: var(--ft_dark);
+    padding: 5px 8px;
+    border-color: var(--ft_cyan);
+    border-radius: 5px;
+  }
+</style>
