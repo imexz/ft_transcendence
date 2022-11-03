93acedba0811f26c1611abfbc6d80fb44bab4657
tobbel42 <tobiasdegrossmann@gmx.de>
2022-11-03 13:47:06 +0100

    overhaul create room Popup

--- src/frontend/src/components/Chat/createRoom.vue
+++ src/frontend/src/components/Chat/createRoom.vue
@@ -1,16 +1,25 @@
 <template>
     <div class="room-input">
       <form @submit.prevent="createRoom">
-        <input v-model="name" />
-        <select v-model="access" >
-          <option :value="Access.private"  >privat</option>
-          <option :value="Access.public" selected>public</option> 
-          <option :value="Access.protected" >protected</option> 
-        </select>
-        <input v-if="access == Access.protected"  v-model="password" placeholder="Enter your password">
-        <button type="submit">Create Room</button>
+        <div class="elem">
+          <input v-model="name" placeholder="Enter the name"/>
+        </div>
+        <div class="elem">
+          <select class="elem2" v-model="access" >
+            <option :value="Access.private"  >privat</option>
+            <option :value="Access.public" selected>public</option> 
+            <option :value="Access.protected" >protected</option> 
+          </select>
+        </div>
+        <div class="elem">
+          <input v-if="access == Access.protected"  v-model="password" placeholder="Enter your password">
+          <input v-else v-model="password" placeholder="Not password protected" disabled>
+        </div>
+        <div class="btn">
+          <button class="elem2" type="submit">Create Room</button>
+        </div>
       </form>
-    </div>    
+    </div>
 </template>
 
 <script lang="ts">
@@ -51,6 +60,15 @@ export default defineComponent({
 </script>
 
 <style scoped>
+
+.elem {
+  margin-top: 2px;
+}
+
+.elem2 {
+  width: 200px;
+}
+
   button {
     color: var(--ft_cyan);
     border: 1px solid var(--ft_cyan);
@@ -70,15 +88,20 @@ export default defineComponent({
     color: var(--ft_cyan);
     background-color: var(--ft_dark);
     padding: 5px 8px;
-    border-color: var(--ft_cyan);
+    border: 1px solid var(--ft_cyan);
     border-radius: 5px;
   }
 
   input{
+    width: 182px;
     color: var(--ft_cyan);
     background-color: var(--ft_dark);
     padding: 5px 8px;
-    border-color: var(--ft_cyan);
+    border: 1px solid var(--ft_cyan);
     border-radius: 5px;
+  }
+
+  .btn {
+    margin-top: 20px;
   }
 </style>
--- src/frontend/src/components/Chat/createRoomPopup.vue
+++ src/frontend/src/components/Chat/createRoomPopup.vue
@@ -1,21 +1,20 @@
 <template>
   <div class="popUp">
-    <div>
-      <h1>Create Room</h1>
-    </div>
-    <createRoom @actions="reEmit"/>
-    <div>
-      <button @click="closePopUp" >
-        Close Popup
+    <div class="headLineWrapper">
+      <div class="headLine">Create a Chatroom</div>
+      <button class="exitButton">
+        <font-awesome-icon icon="fa-solid fa-x" />
       </button>
     </div>
+    <createRoom @actions="reEmit"/>
   </div>
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue';
 import createRoom from './createRoom.vue';
 
-export default {
+export default defineComponent({
   components: {
     createRoom,
   },
@@ -28,7 +27,7 @@ export default {
       this.$emit('actions', emitMsg)
     }
   }
-}
+})
 </script>
 
 <style scoped>
@@ -39,32 +38,43 @@ export default {
     right: 0;
     top: 300px;
     width: 400px;
-    height: 200px;
+    height: 230px;
     background-color: var(--ft_dark);
     border: 1px solid var(--ft_cyan);
     border-radius: 10px;
     z-index: 10;
 
-    /* display: flex; */
-    align-items: center;
-    justify-content: center;
   }
-  div {
-    margin-top: 20px
+
+  .headLineWrapper {
+    margin-top: 15px;
+    margin-bottom: 15px;
+    padding-bottom: 10px;
+    padding-left: 15px;
+    padding-right: 15px;
+    border-bottom: 1px solid var(--ft_cyan);
+    display: flex;
+    justify-content: space-between;
   }
-  button {
-    color: var(--ft_cyan);
-    border: 1px solid var(--ft_cyan);
-    border-radius: 5px;
+
+  .headLine {
+    font-size: 25px;
+    font-weight: bold;
+  }
+
+  .exitButton {
+    height: 30px;
+    width: 30px;
+    font-weight: bold;
+    padding: 3px;
+    border-radius: 50%;
+    border: 2px solid var(--ft_pink);
+    color: var(--ft_pink);
     background-color: var(--ft_dark);
-    padding: 5px 8px;
   }
-  button:active {
-    transform: translateY(1px);
-  }
-  button:hover {
+  .exitButton:hover {
     color: var(--ft_dark);
-    background-color: var(--ft_cyan);
+    background-color: var(--ft_pink);
   }
 
 </style>
